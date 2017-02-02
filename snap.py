ad928f03fae6410ba2b855bd94bf8dce9e5c1e06
Margarita Villarreal <maggsvisan@users.noreply.github.com>
2017-02-02 15:46:02 +0000

    New python script!
    Este script no muestra la foto leída, solo guarda

---
+++ snap.py
@@ -0,0 +1,42 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import math
+ 
+# initialize the camera and grab a reference to the raw camera capture
+#create Camera Object
+camera = PiCamera() 
+camera.resolution=(640,480)
+camera.framerate=32
+rawCapture = PiRGBArray(camera, size=(640,480))
+
+key= cv2.waitKey(1) & 0xFF
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+while (True):
+
+    data= int(input("enter a number "))
+  #  print "Input data",data
+    
+    # grab an image from the camera
+    if (data == 10):
+        camera.capture(rawCapture, format="bgr")
+        print ("Taking snap")
+        image = rawCapture.array
+        #save an image to current path
+        cv2.imwrite("Snap.jpg",image)
+        #clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        
+     #   cv2.waitKey(0)
+
+    elif key == ord("q"):
+        break
+    
+    else:
+         print ("Enter another number to take snap")
+
